4d2a0d079590b98e72d93148561b607a84382bc5
Esemega <sogaral89@gmail.com>
2020-04-29 20:17:45 +0200

    created loadUser function, connecting Rank, SignIn and Register with backend

--- src/App.js
+++ src/App.js
@@ -34,8 +34,28 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
+  }
+
+  loadUser = (data) => {
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined
+      }
+    })
+
   }
 
   onInputChange = (event) => {
@@ -91,7 +111,9 @@ class App extends Component {
         ? 
         <div>
           <Logo/>
-          <Rank/>
+          <Rank 
+            name={this.state.user.name} 
+            entries={this.state.user.entries}/>
           <ImageLinkForm 
             onInputChange={this.onInputChange} 
             onButtonSubmit= {this.onButtonSubmit}
@@ -100,8 +122,8 @@ class App extends Component {
         </div>
         : (
           route === 'signin'
-          ? <SignIn onRouteChange={this.onRouteChange} />
-          : <Register onRouteChange={this.onRouteChange} />
+          ? <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+          : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
           )
         }
       </div>
